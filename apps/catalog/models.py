89128be066397b03bff38eb3677d65ce19d74139
max <arnautmax10@gmail.com>
2026-05-06 17:27:45 +0300

    Catalog app update

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# model for saving movies
+class Movie(models.Model):
+    GENRE_CHOICES = [
+        ('action', 'Action'),
+        ('comedy', 'Comedy'),
+        ('drama', 'Drama'),
+        ('horror', 'Horror'),
+        ('sci-fi', 'Sci-Fi'),
+        ('thriller', 'Thriller'),
+        ('romance', 'Romance'),
+        ('documentary', 'Documentary'),
+    ]
+
+    title = models.CharField(max_length=70)
+    description = models.TextField()
+    image = models.ImageField(upload_to='posters/')
+    genre = models.CharField(max_length=50, choices=GENRE_CHOICES)
+    duration = models.IntegerField(blank=True, null=True)
+    release_year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} - {self.release_year}" 
--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
+
+def home(request):
+    genre_filter = request.GET.get('genre', '')
+    movies = Movie.objects.all()
+
+    if genre_filter:
+        movies = movies.filter(genre=genre_filter)
+
+    genres = Movie.GENRE_CHOICES
+
+    return render(request, 'catalog/main.html', {
+        'movies': movies,
+        'genres': genres,
+        'active_genres': genre_filter,
+    })
