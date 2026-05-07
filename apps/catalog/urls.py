54cb6461b6e9fefb6c30ce46825beaf917cfdaba
max <arnautmax10@gmail.com>
2026-05-07 16:26:43 +0300

    feat: Add search

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('new/', views.new_movies, name='new_release'),
     path('watchlist/', views.watchlist, name='watchlist'),
     path('favorites/<int:movie_id>/', views.toggle_favorites, name='toggle_favorite'),
+    path('search/', views.search, name='search')
 ]
--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -3,6 +3,7 @@ from .models import Movie, Favorite
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+import random
 
 # Create your views here.
 
@@ -15,10 +16,20 @@ def home(request):
 
     genres = Movie.GENRE_CHOICES
 
+    favorite_ids = set()
+    if request.user.is_authenticated:
+        favorite_ids = set(
+            Favorite.objects.filter(user=request.user).values_list('movie_id', flat=True)
+        )
+
+    hero_movie = random.choice(list(Movie.objects.all()))
+
     return render(request, 'catalog/main.html', {
         'movies': movies,
         'genres': genres,
         'active_genres': genre_filter,
+        'favorite_ids': favorite_ids,
+        'hero_movie': hero_movie,
     })
 
 def new_movies(request):
@@ -28,6 +39,7 @@ def new_movies(request):
     movies = Movie.objects.filter(release_year__gte=new).order_by('-release_year')
     return render(request, 'catalog/new_release.html', {'movies': movies},)
 
+# view for adding to favorites
 @login_required
 def toggle_favorites(request, movie_id):
     movie = get_object_or_404(Movie, id=movie_id)
@@ -41,7 +53,25 @@ def toggle_favorites(request, movie_id):
 
     return JsonResponse({'is_favorite': is_favorite, 'movie_id': movie_id})
 
+# load Favorites page
 @login_required
 def watchlist(request):
     favorites = Favorite.objects.filter(user=request.user).select_related('movie').order_by('-added_at')
     return render(request, 'catalog/watchlist.html', {'favorites': favorites})
+
+
+# view for search
+def search(request):
+    query = request.GET.get('q', '')
+    movies = Movie.objects.none()
+
+    if query:
+        movies = Movie.objects.filter(title__icontains=query)
+
+    favorite_ids = set()
+    if request.user.is_authenticated:
+        favorite_ids = set(
+            Favorite.objects.filter(user=request.user).values_list('movie_id', flat=True)
+        )
+    
+    return render(request, 'catalog/search.html', {"movies": movies, "query": query, 'favorite_ids': favorite_ids})
