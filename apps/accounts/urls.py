143c6498f0afebaa125a3fac3909e28f0ff4ef9e
max <arnautmax10@gmail.com>
2026-05-13 11:52:33 +0300

    feat: Added profile

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/',    views.login_view,    name='login'),
-    path('logout/',   views.logout_view,   name='logout'),
+    path('login/', views.login_view,    name='login'),
+    path('logout/', views.logout_view,   name='logout'),
     path('register/', views.register_view, name='register'),
+    path('profile/', views.profile_view, name='profile'),
+    path('change-password/', views.change_password_view, name='change_password'),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout, update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
 from django.contrib import messages
+from apps.catalog.models import Favorite
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -49,3 +51,38 @@ def register_view(request):
         form = UserCreationForm()
 
     return render(request, 'accounts/register.html', {"form": form})
+
+# datele pentru pagina Profile
+@login_required
+def profile_view(request):
+
+    recent_favorites = (Favorite.objects.filter(user=request.user).select_related('movie').order_by('-added_at')[:6])
+
+    from django.db.models import Count
+    top_genre = (Favorite.objects.filter(user=request.user).values('movie__genre').annotate(count=Count('id')).order_by('-count').first())
+
+    favorite_genre = top_genre['movie__genre'].capitalize() if top_genre else None
+
+    return render(request, 'accounts/profile.html', {
+        'recent_favorites': recent_favorites,
+        'favorite_genre': favorite_genre,
+        'total_favorites': Favorite.objects.filter(user=request.user).count(),
+    })
+
+@login_required
+def change_password_view(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Parola modificata cu succes.')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Corecteaza erorile de mai jos.')
+    
+    else:
+        form = PasswordChangeForm(request.user)
+
+    return render(request, 'accounts/change_password.html', {'form': form})
